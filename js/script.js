524e1e7abf20219f75a56a4641c30260c461a030
Gerardo Garavito <garavitogerardo@gmail.com>
2020-08-07 09:04:55 -0500

    challenge done

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function toggle() {
+  var banner = document.getElementById('banner')
+  
+  if (banner.style.display === 'none'){
+    banner.style.display = 'flex'
+  }else {
+    banner.style.display = 'none'
+  }
+}
